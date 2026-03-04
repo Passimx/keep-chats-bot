32686855db7b446f498cca8186c43873ea2e9415
ramzini22 <ramil.zinnatullin.02@mail.ru>
2026-03-04 12:19:26 +0800

    refactor: add video

--- src/modules/telegram/telegram-service.ts
+++ src/modules/telegram/telegram-service.ts
@@ -32,8 +32,8 @@ export class TelegramService {
     [
       Markup.button.callback('🌐️ Меню', 'BTN_1'),
       Markup.button.callback('📖 Инструкция', 'BTN_4'),
-      Markup.button.url('👩‍💻 Поддержка', 'https://t.me/passimx'),
     ],
+    [Markup.button.url('👩‍💻 Поддержка', 'https://t.me/passimx')],
     [
       Markup.button.url(
         '📄 Пользовательское соглашение',
