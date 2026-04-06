95d21c866cd5998cc6f75af10397997be5aabc36
ramzini22 <ramil.zinnatullin.02@mail.ru>
2026-04-06 10:39:00 +0800

    refactor: add table actions

--- src/modules/database/entities/action.entity.ts
+++ src/modules/database/entities/action.entity.ts
@@ -1,8 +1,11 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { UpdateType } from '../../telegram/types/update.type';
 
 @Entity({ name: 'actions' })
 export class ActionEntity {
-  @Column({ name: 'info', type: 'jsonb', primary: true })
+  @PrimaryGeneratedColumn('rowid')
+  readonly id: string;
+
+  @Column({ name: 'info', type: 'jsonb' })
   readonly info: UpdateType;
 }
